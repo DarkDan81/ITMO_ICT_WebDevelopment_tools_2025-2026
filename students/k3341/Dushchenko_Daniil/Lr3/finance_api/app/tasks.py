7bf68b3d8aad26e5062f527d713a490311c7a2d4
Dushchenko Daniil <dusyadusa@gmail.com>
2026-03-30 00:37:09 +0300

    Prepare branch for laboratory work 2 submission

--- students/k3341/Dushchenko_Daniil/Lr3/finance_api/app/tasks.py
+++
@@ -1,8 +0,0 @@
-from app.celery_app import celery_app
-from app.parser_client import parse_via_service
-from app.parser_models import ParseRequest
-
-
-@celery_app.task(name="app.tasks.parse_url_task")
-def parse_url_task(url: str) -> dict:
-    return parse_via_service(ParseRequest(url=url))
